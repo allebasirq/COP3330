ef3e6d390a4f26e036b8493f67115f0a71aac88a
Isabella Quesada <allebasirq@gmail.com>
2025-12-27 16:17:30 -0500

    Add files via upload

---
+++ Dice.java
@@ -0,0 +1,34 @@
+// Author: Isabella Rodrigues Quesada
+// Assignment 2
+// Note: This program uses a Die class to create a Dice object. It uses randomness to roll the die and gets the sum of the faces for the dice.
+// Finally, it outputs the frequency of sum of the dice in formatted manner and illustrates a histogram based on the data.
+
+package assignment2;
+
+public class Dice {
+
+	private Die firstDie, secondDie;
+	
+	// No argument constructor
+	public Dice() {
+		
+		firstDie = new Die();								// Initialize firstDie and secondDie
+		secondDie = new Die();
+		
+		firstDie.setFaceValue(1);
+		secondDie.setFaceValue(1);
+		
+	}
+	
+	public int sumFaceValues() {
+		
+		return firstDie.getFaceValue() + secondDie.getFaceValue();						// Returns the sum of faceValues
+	}
+	
+	// Method to run both dice
+	public void rollDice() {
+		
+		firstDie.roll();
+		secondDie.roll();
+	}
+}
---
+++ Die.java
@@ -0,0 +1,38 @@
+// Author: Isabella Rodrigues Quesada
+// Assignment 2
+// Note: This program uses a Die class to create a Dice object. It uses randomness to roll the die and gets the sum of the faces for the dice.
+// Finally, it outputs the frequency of sum of the dice in formatted manner and illustrates a histogram based on the data.
+
+package assignment2;
+
+import java.util.Random;
+
+public class Die {
+
+	private int faceValue;
+	
+	public Die() {									// No argument constructor that sets faceValue to 1
+		faceValue = 1;		
+	}
+	
+	public int getFaceValue() {						// Getter method to get faceValue
+		return faceValue;
+	}
+	
+	public void setFaceValue(int givenFaceValue) {				// Setter method to set faceValue with given parameter
+		faceValue = givenFaceValue;
+	}
+	
+	public void roll() {
+		
+		Random random = new Random();
+		
+		int upperbound = 6;
+		
+		int newFace = random.nextInt(1, upperbound+1);			// New face generates [1,7): meaning 1 to 6
+		
+		faceValue = newFace;
+		
+	}
+	
+}
